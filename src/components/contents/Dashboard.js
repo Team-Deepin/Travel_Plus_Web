cf503c425bebe248a43e1cc6767a46fcc42d6b34
Wol-haseo <iksyi030212@gmail.com>
2025-04-08 14:27:08 +0900

    대시보드 수정

--- src/components/contents/Dashboard.js
+++ src/components/contents/Dashboard.js
@@ -19,19 +19,26 @@ const DashboardPage = () => {
   return (
     <div className="dashboard-content">
       <div className="card-row">
-        <div className="card card-1">
-          <p className="card-text">이번 주 활성 사용자 수</p>
-          {console.log('todo', todo ? todo.title : '로딩 중')}
+        <div className="column-wrap">
+          <div className="card card-1">
+                <p className="card-text">전체 사용자</p>
+          </div>
+          <div className="card card-1">
+            <p className="card-text">현재 접속자</p>
+          </div>
         </div>
-      </div>
 
-      <div className="card-row">
+        <div className="column-wrap">
+          <div className="card card-1">
+            <p className="card-text">7일 이내 신규 가입자</p>
+          </div>
+          <div className="card card-1">
+            <p className="card-text">3일 이내 탈퇴 회원</p>
+          </div>
+        </div>
+
         <div className="card card-2">
-          <p className="card-text">누적 이용자 수</p>
-        </div>
-
-        <div className="card card-3">
-          <p className="card-text">이번 주 신규 가입자</p>
+          <p className="card-text">문의 통계</p>
         </div>
       </div>
     </div>
